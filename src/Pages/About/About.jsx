7df1ac082ad88f483e79dce3456151bad45e50e0
Kartik <kr.pjpt@gmail.com>
2022-07-12 00:10:21 +0530

    a

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,156 +1,121 @@
 import React from "react";
-import {Helmet} from 'react-helmet';
+import { Helmet } from "react-helmet";
+import search from "../../assets/search.gif";
+import predict from "../../assets/predict.png";
 
-const About = ({theme}) => {
-  console.log(theme);
-  return (
-    <div className="aboutgemx">
-    <Helmet>
-        <title>StockMonkey | About</title>
-      </Helmet>
-      {theme==="light" ? (
-        <>
-          <h1>About StockMonkey</h1>
-      <div className="gemx">
-       <div >
-       <img
-          src="https://s3-alpha-sig.figma.com/img/fc5e/fc21/f32d3c6e45510b4964901662e7626e66?Expires=1647216000&Signature=EdrMKZV5dIkaJhCIKnT5FWSpqH~itLq7rL3W8fSwZ4Jf0Ffu19SMgWFyR2cB940P-YYGhFMlkaT0HGYV4nYLoo888be~PnuUEl86hpMhTlxHhFG3C5oBBCP2QqD8tSzPaVQQ8NlhRlBnM6gzHT5lJ6KZ-OhWXJfARAHI9pN-Gd22t-PXZX4KQo8SND3RTCOaQl2XfK9FrxcGMbtwaaj41DFaBV5n2Fo0bnO5Z6eG0eXJQzcqTL2GcUpIhkZiE9g0-w-C0~62jvU7nn7ceCEixqf11j3bs95x4iAwbY4zqxBhfu2nq9mRK6TZHQvww0h07dYzTr1RL25VrdLIHsWcSw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-          alt=""
-        />
-       </div>
-      </div>
+const About = ({ theme }) => {
+    console.log(theme);
+    return (
+        <div className="aboutgemx">
+            <Helmet>
+                <title>StockMonkey | About</title>
+            </Helmet>
+            {theme === "light" ? (
+                <>
+                    <h1>About StockMonkey</h1>
+                    <div className="gemx">
+                        <div>
+                            <img
+                                src="https://miro.medium.com/max/1400/1*NG0bzk0wtQcBdMYAnXKeBQ.jpeg"
+                                alt=""
+                            />
+                        </div>
+                    </div>
 
-      <p className="content1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut <br />{" "}
-        aliquam, purus sit amet luctus{" "}
-      </p>
-      <h3 className="heading1">Crypto for Creative Communities</h3>
-      <p className="content2">
-        NFTs—non-fungible tokens—are empowering artists, musicians, and <br />{" "}
-        all kinds of genre-defying digital creators to invent a new cultural{" "}
-        <br /> paradigm. How this emerging culture of digital art ownership
-        looks is <br /> up to all of us.
-      </p>
-      <h3 className="heading2">How it work</h3>
-      <div className="images">
-        
-          <div className="image1">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/3d8d/8c57/760a773d843e72baa031ce746593017a?Expires=1647216000&Signature=LV~lmQkB76dLriUWQehw-qoFyPJyuVKDod-eAe9BDFaaIlso40oTrwmJsj-VKpQk9IYiEd7SYMSJhJS~Oqxl5VSF-eKoGZXA0pIyTlwNci2CTN0pCpZ-22hrUN4WrK~K8auhwLE~r8V98xiQTcTShWcETTDspSLdx2O0wQ032vAI54NPUPGf2bdm83e0YoMXpAIqv1L8hm0iL~dXdg2-PWTVDH7vtYS533HcUwQsejshbz26BWRsoFKR0h0g~CRXz3h4jsDbtj-NSmXE5toyWX7qTEC58fi~CUWg9BlVXsQIaC-mm7bJSp0xhQ2N5X6vOZMXwJ227jPdzUc~SqxmOw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-              alt=""
-            />
-          <h3 > Build together</h3>
-          </div>
-        
-        
-          <div className="image2">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/4f11/9366/3dc1f79f19d5cdec38f1730881c6c9f8?Expires=1647216000&Signature=CC3FAvxyI-GQ9pXe~Iou92wn1SPfL65ibzHuNEaLRbCgJOwBvjy5BwZwbpmBL~hQyMD6u8O1yCBHWqbH5L23-MTk6oE2x7Fo70hel61aXJFXeAkjY~wPu2H8khDIZlJELzH7pXCuhUNPhOUHl5Tm4J5UVcQhfmXc~qcyahcbCKHvnsIKN7~Y53KOjh9ZcpL2niILwPsnpcOPcfwS-u~OKV3qSvBINtQtyUbKFxQLjLjhZW8Gb7ExF~LXHD41KaqxJTQZtrqWA3Orn~EDSQeGkkdcaQj5hT1bwShycoBniuy3vbdaaBtpIl1VFFNeUWIkVeRbSMVQG4SCgFg5z94bBg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-              alt=""
-            />
-          <h3>Trust</h3>
-          </div>
-        </div>
-      
+                    <p className="content1">
+                        A stock market is a public market where you can buy and
+                        sell shares for publicly listed companies. The stocks,
+                        also known as equities, represent ownership in the
+                        company. The stock exchange is the mediator that allows
+                        the buying and selling of shares. StockMonkey helps in
+                        prediction of stock market using machine learning
+                        models.
+                    </p>
+                    <h3 className="heading1">
+                        StockMonkey for Creative Communities
+                    </h3>
+                    <p className="content2">
+                        Stock Price Prediction using machine learning helps you
+                        discover the future value of company stock and other
+                        financial assets traded on an exchange. The entire idea
+                        of predicting stock prices is to gain significant
+                        profits. Predicting how the stock market will perform is
+                        a hard task to do. There are other factors involved in
+                        the prediction, such as physical and psychological
+                        factors, rational and irrational behavior, and so on.
+                        All these factors combine to make share prices dynamic
+                        and volatile. This makes it very difficult to predict
+                        stock prices with high accuracy.
+                    </p>
+                    <h3 className="heading2">How it work</h3>
+                    <div className="images">
+                        <div className="image1">
+                            <img src={search} alt="" />
+                            <h3>Search Stock</h3>
+                        </div>
 
-      <div className="flexbox1">
-        <div>
-          <h3>For Creators</h3>
-          <p>
-            Creators are invited to join Foundation by members of the community.
-            Once you’ve received an invite, you’ll need to set up a MetaMask
-            wallet with ETH before you can create an artist profile and mint an
-            NFT—which means uploading your JPG, PNG, or video file to IPFS, a
-            decentralized peer-to-peer storage network. It will then be an NFT
-            you can price in ETH and put up for auction on Foundation.
-          </p>
-        </div>
-        <div>
-          <h3>For Collectors</h3>
-          <p>
-            On Foundation, anyone can create a profile to start collecting NFTs.
-            All you’ll need is a MetaMask wallet and ETH, the cryptocurrency
-            used to pay for all transactions on Ethereum. Artists list NFTs for
-            auction at a reserve price, and once the first bid is placed, a
-            24-hour auction countdown begins. If a bid is placed within the last
-            15 minutes, the auction extends for another 15 minutes.
-          </p>
-        </div>
-      </div>
-        </>
-      ):(
-        <>
-        <h1>About StockMonkey</h1>
-      <div className="gemx">
-       <div >
-       <img
-          src="https://s3-alpha-sig.figma.com/img/fc5e/fc21/f32d3c6e45510b4964901662e7626e66?Expires=1647216000&Signature=EdrMKZV5dIkaJhCIKnT5FWSpqH~itLq7rL3W8fSwZ4Jf0Ffu19SMgWFyR2cB940P-YYGhFMlkaT0HGYV4nYLoo888be~PnuUEl86hpMhTlxHhFG3C5oBBCP2QqD8tSzPaVQQ8NlhRlBnM6gzHT5lJ6KZ-OhWXJfARAHI9pN-Gd22t-PXZX4KQo8SND3RTCOaQl2XfK9FrxcGMbtwaaj41DFaBV5n2Fo0bnO5Z6eG0eXJQzcqTL2GcUpIhkZiE9g0-w-C0~62jvU7nn7ceCEixqf11j3bs95x4iAwbY4zqxBhfu2nq9mRK6TZHQvww0h07dYzTr1RL25VrdLIHsWcSw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-          alt=""
-        />
-       </div>
-      </div>
+                        <div className="image2">
+                            <img
+                                src={predict}
+                                alt=""
+                            />
+                            <h3>Predict</h3>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <h1>About StockMonkey</h1>
+                    <div className="gemx">
+                        <div>
+                            <img src={predict} alt="" />
+                        </div>
+                    </div>
 
-      <p className="content3">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut <br />{" "}
-        aliquam, purus sit amet luctus{" "}
-      </p>
-      <h3 className="heading3">Crypto for Creative Communities</h3>
-      <p className="content4">
-        NFTs—non-fungible tokens—are empowering artists, musicians, and <br />{" "}
-        all kinds of genre-defying digital creators to invent a new cultural{" "}
-        <br /> paradigm. How this emerging culture of digital art ownership
-        looks is <br /> up to all of us.
-      </p>
-      <h3 className="heading4">How it work</h3>
-      <div className="images">
-        
-          <div className="image3">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/3d8d/8c57/760a773d843e72baa031ce746593017a?Expires=1647216000&Signature=LV~lmQkB76dLriUWQehw-qoFyPJyuVKDod-eAe9BDFaaIlso40oTrwmJsj-VKpQk9IYiEd7SYMSJhJS~Oqxl5VSF-eKoGZXA0pIyTlwNci2CTN0pCpZ-22hrUN4WrK~K8auhwLE~r8V98xiQTcTShWcETTDspSLdx2O0wQ032vAI54NPUPGf2bdm83e0YoMXpAIqv1L8hm0iL~dXdg2-PWTVDH7vtYS533HcUwQsejshbz26BWRsoFKR0h0g~CRXz3h4jsDbtj-NSmXE5toyWX7qTEC58fi~CUWg9BlVXsQIaC-mm7bJSp0xhQ2N5X6vOZMXwJ227jPdzUc~SqxmOw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-              alt=""
-            />
-          <h3 > Build together</h3>
-          </div>
-        
-        
-          <div className="image4">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/4f11/9366/3dc1f79f19d5cdec38f1730881c6c9f8?Expires=1647216000&Signature=CC3FAvxyI-GQ9pXe~Iou92wn1SPfL65ibzHuNEaLRbCgJOwBvjy5BwZwbpmBL~hQyMD6u8O1yCBHWqbH5L23-MTk6oE2x7Fo70hel61aXJFXeAkjY~wPu2H8khDIZlJELzH7pXCuhUNPhOUHl5Tm4J5UVcQhfmXc~qcyahcbCKHvnsIKN7~Y53KOjh9ZcpL2niILwPsnpcOPcfwS-u~OKV3qSvBINtQtyUbKFxQLjLjhZW8Gb7ExF~LXHD41KaqxJTQZtrqWA3Orn~EDSQeGkkdcaQj5hT1bwShycoBniuy3vbdaaBtpIl1VFFNeUWIkVeRbSMVQG4SCgFg5z94bBg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
-              alt=""
-            />
-          <h3 >Trust</h3>
-          </div>
-        </div>
-      
+                    <p className="content3">
+                        A stock market is a public market where you can buy and
+                        sell shares for publicly listed companies. The stocks,
+                        also known as equities, represent ownership in the
+                        company. The stock exchange is the mediator that allows
+                        the buying and selling of shares. StockMonkey helps in
+                        prediction of stock market using machine learning
+                        models.{" "}
+                    </p>
+                    <h3 className="heading3">
+                        Crypto for Creative Communities
+                    </h3>
+                    <p className="content4">
+                        Stock Price Prediction using machine learning helps you
+                        discover the future value of company stock and other
+                        financial assets traded on an exchange. The entire idea
+                        of predicting stock prices is to gain significant
+                        profits. Predicting how the stock market will perform is
+                        a hard task to do. There are other factors involved in
+                        the prediction, such as physical and psychological
+                        factors, rational and irrational behavior, and so on.
+                        All these factors combine to make share prices dynamic
+                        and volatile. This makes it very difficult to predict
+                        stock prices with high accuracy.
+                    </p>
+                    <h3 className="heading4">How it work</h3>
+                    <div className="images">
+                        <div className="image3">
+                            <img src={predict} alt="" />
+                            <h3>Search Stock</h3>
+                        </div>
 
-      <div className="flexbox2">
-        <div>
-          <h3>For Creators</h3>
-          <p>
-            Creators are invited to join Foundation by members of the community.
-            Once you’ve received an invite, you’ll need to set up a MetaMask
-            wallet with ETH before you can create an artist profile and mint an
-            NFT—which means uploading your JPG, PNG, or video file to IPFS, a
-            decentralized peer-to-peer storage network. It will then be an NFT
-            you can price in ETH and put up for auction on Foundation.
-          </p>
+                        <div className="image4">
+                            <img
+                                src="https://s3-alpha-sig.figma.com/img/4f11/9366/3dc1f79f19d5cdec38f1730881c6c9f8?Expires=1647216000&Signature=CC3FAvxyI-GQ9pXe~Iou92wn1SPfL65ibzHuNEaLRbCgJOwBvjy5BwZwbpmBL~hQyMD6u8O1yCBHWqbH5L23-MTk6oE2x7Fo70hel61aXJFXeAkjY~wPu2H8khDIZlJELzH7pXCuhUNPhOUHl5Tm4J5UVcQhfmXc~qcyahcbCKHvnsIKN7~Y53KOjh9ZcpL2niILwPsnpcOPcfwS-u~OKV3qSvBINtQtyUbKFxQLjLjhZW8Gb7ExF~LXHD41KaqxJTQZtrqWA3Orn~EDSQeGkkdcaQj5hT1bwShycoBniuy3vbdaaBtpIl1VFFNeUWIkVeRbSMVQG4SCgFg5z94bBg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                                alt=""
+                            />
+                            <h3>Trust</h3>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
-        <div>
-          <h3>For Collectors</h3>
-          <p>
-            On Foundation, anyone can create a profile to start collecting NFTs.
-            All you’ll need is a MetaMask wallet and ETH, the cryptocurrency
-            used to pay for all transactions on Ethereum. Artists list NFTs for
-            auction at a reserve price, and once the first bid is placed, a
-            24-hour auction countdown begins. If a bid is placed within the last
-            15 minutes, the auction extends for another 15 minutes.
-          </p>
-        </div>
-      </div>
-        </>
-      )}
-    </div>
-  );
+    );
 };
 
 export default About;
